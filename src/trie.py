38fc2d03359c7bf41996666ef77ab3008d5f95a4
Jakub Duchon <jakub@duchy.sk>
2024-03-18 17:42:30 +0000

    feat: increase TrieNode occurrencies when calling Trie.insert

--- src/trie.py
+++ src/trie.py
@@ -1,23 +1,31 @@
+from typing import Self
+
+
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.occurences = 0
-        self.is_end = False
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+    @staticmethod
+    def from_list(iam_actions: list[str]) -> Self:
+        trie = Trie()
+
+        for action in iam_actions:
+            trie.insert(action)
+
+        return trie
+
     def insert(self, word):
         node = self.root
 
         for char in word:
-            node.occurences += 1
-
             if char not in node.children:
                 node.children[char] = TrieNode()
 
             node = node.children[char]
-
-        node.is_end = True
+            node.occurences += 1
